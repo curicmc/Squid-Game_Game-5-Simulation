02514fabb419b642d625303be5b547ea1f1ebfc3
curicmc <curicmc@gmail.com>
2021-10-01 01:23:43 -0700

    Added probability of # of winners to runBridgeAverage()

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,14 @@
 package com.company;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class Main {
 
     public static void main(String[] args) {
 
         //Returns the statistics of a bridge game run 1000 times with 16 players and a bridge length of 18
-        runBridgeAverage(16, 18, 1000);
+        runBridgeAverage(16, 18, 10000);
 
         //Runs a step-by-step simulation of a single game with 16 players and a bridge length of 18
         BridgeGame originalGame = new BridgeGame(16, 18);   // creates bridge game
@@ -16,8 +19,8 @@ public class Main {
 
     /** Runs the bridge game x (loop) number of times with specified parameters and provides average
      * number of players that win, as well as a breakdown of the amount of winners of each game
-     * @param players
-     * @param bridgeLength
+     * @param players; players in the game
+     * @param bridgeLength; pairs of steps in the bridge
      * @param loop; number of games to play
      * @return average amount of players won
      */
@@ -41,11 +44,12 @@ public class Main {
         System.out.println("\nAverage players left (players: " + players + ", bridge length: " + bridgeLength + "): " + averageLeft);
 
         System.out.println("Statistics for " + loop + " game(s):");
-        System.out.println("\t| Players Remaining : Count |");
+        System.out.println("\t| Players Remaining : Count : Probability |");
         System.out.println("\t------------------------------");
         for(int i = 0; i <= players; i++){
-            System.out.println("\t" + i + "\t : " + playersLeftCount[i]);
-
+            BigDecimal probability = BigDecimal.valueOf(playersLeftCount[i]).divide(BigDecimal.valueOf(loop), 10, RoundingMode.HALF_UP);
+            System.out.println("\t" + i + "\t : " + playersLeftCount[i] + "\t : "
+                    + probability.multiply(BigDecimal.valueOf(100)).setScale(2, RoundingMode.HALF_UP) + "%");
         }
         return averageLeft;
     }
